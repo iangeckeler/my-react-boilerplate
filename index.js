94ea0d4da9bd33122173ddd15b61bb82e0d7f981
Ian Geckeler <ikcgeckeler@gmail.com>
2019-03-11 10:13:44 -0700

    added state to app component

--- index.js
+++ index.js
@@ -5,4 +5,25 @@ const Index = () => {
   return <div>Hello React!</div>;
 };
 
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            stateEnabled: true
+        }
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h1>Hello React!</h1>
+            </div>
+        )
+    };
+
+
+}
+
 ReactDOM.render(<Index />, document.getElementById("index"));
